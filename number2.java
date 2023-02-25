a361af0157b58ab565ad3077b0d3b4250c25c656
Harsh Sharma <117378407+GitHub-Harsh-Sharma@users.noreply.github.com>
2023-02-25 18:19:05 +0530

    Add files via upload

---
+++ number2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+class number2
+{
+ public static void main(String[] args)
+  {
+    int i,k,a=1;
+    for(i=1; i<=3;i++)
+    {
+        for(k=1;k<=a;k++)
+        {
+            System.out.print(i);
+        }
+        System.out.println("");
+        a=a+2;
+    }
+        
+    }
+}
